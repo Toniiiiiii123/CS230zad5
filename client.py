fbd244bd471de21fcd55ffc0a7374c2545368ee9
Toniiiiiii123 <antonije.cvetanovic.6442@metropolitan.ac.rs>
2026-09-21 12:03:20 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,1 +1,17 @@
+import socket
 
+HOST = '127.0.0.1'
+MIDDLEWARE_PORT = 12345
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, MIDDLEWARE_PORT))
+
+zahtev = input("Unesite zahtev: ")
+
+client_socket.sendall(zahtev.encode())
+
+odgovor = client_socket.recv(1024).decode()
+
+print("Odgovor:", odgovor)
+
+client_socket.close()
